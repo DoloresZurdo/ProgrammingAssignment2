20f7c5d20312edcbb67a4989fe070a6a5c14273f
DoloresZurdo <dzcnekane@gmail.com>
2014-04-26 20:29:09 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,13 +2,16 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv<- NULL # I stablish inv variable as "inverse" with null value
-  set<<-function(B){ #I set the value of the matrix as the matrix is introduce to call makeCacheMatrix
+  set<<-function(B){ #I set the value of the matrix as the matrix 
+                     # is introduce to call makeCacheMatrix
     B<<-X
     inv<<-NULL
   }
-  get<-function()X #I get the function and return the value of the matrix which call makeCacheMatrix
+  get<-function()X #I get the function and return the value of 
+                   # the matrix which call makeCacheMatrix
   #Inv is the variable for inverse
-  setinverse<- function(inverse) inv<<-inverse(A) #setinverse is a function which set the inverse of the matrix. 
+  setinverse<- function(inverse) inv<<-inverse(A) # setinverse is a function which 
+                                                  # set the inverse of the matrix. 
   getinverse<- function() inv #getinverse get the function inverse of the matrix
   list(set=set, get=get, 
        setinverse=setinverse,
@@ -20,8 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-inv<-X$setinverse() #hte function search if there is an inverse saved in cache memory
-  if(!is.null(X)){ #if there is not an inverse matrix in cache memeory then it will pop-up a message indicating is getting cache matrix
+inv<-X$setinverse() # the function search if there is 
+                    # an inverse saved in cache memory
+  if(!is.null(X)){ #if there is not an inverse matrix in cache 
+                   # memeory then it will pop-up a message indicating 
+                   # is getting cache matrix
     messge("getting cache matrix")
     return(inv) #it will return the inverse matrix
   }
