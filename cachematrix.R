dc7bc2fbca3bf6fc0ec954a9057a405de594fef2
DoloresZurdo <dzcnekane@gmail.com>
2014-04-27 14:19:15 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,21 +2,23 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv<- NULL # I stablish inv variable as "inverse" with null value
-  set<<-function(B){ #I set the value of the matrix as the matrix 
-                     # is introduce to call makeCacheMatrix
-    B<-X
+  set<-function(B){ #I set the value of the matrix as the matrix 
+    # is introduce to call makeCacheMatrix
+    x<<-B
     inv<<-NULL
   }
-  get<-function()X #I get the function and return the value of 
-                   # the matrix which call makeCacheMatrix
+  get<-function()x #I get the function and return the value of 
+  # the matrix which call makeCacheMatrix
   #Inv is the variable for inverse
-  setinverse<- function(inverse) inv<<-inverse(A) # setinverse is a function which 
-                                                  # set the inverse of the matrix. 
+  setinverse<- function(inverse) {inv<<-inverse # setinverse is a function which 
+                                                # set the inverse of the matrix.
+                                  
+  } 
   getinverse<- function() inv #getinverse get the function inverse of the matrix
   list(set=set, get=get, 
        setinverse=setinverse,
        getinverse=getinverse)
-
+  
 }
 
 
